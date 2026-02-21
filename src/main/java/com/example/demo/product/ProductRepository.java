e91d27474e963deb452620c4eaf97e0926a2371b
Ahgyeong <beryl61@gmail.com>
2026-02-21 17:08:17 +0900

    feat : ProductRepository 생성
    (cherry picked from commit 3247b9bfef17c78ef8ea94fa8e6619ebb509a07d)

---
+++ src/main/java/com/example/demo/product/ProductRepository.java
@@ -0,0 +1,21 @@
+package com.example.demo.product;
+
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class ProductRepository {
+
+    private Map<Integer, Product> db = new HashMap<>();
+    private int id = 1;
+
+    public Product getProduct(int id){
+        return db.get(id);
+    }
+
+    public void saveProduct(Product product){
+        db.put(id++,product);
+    }
+}
