b7ebb2593cf1c8097b42aae784c302ecd1340a6f
Ahgyeong <beryl61@gmail.com>
2026-02-21 17:09:09 +0900

    feat : ProductService 생성

---
+++ src/main/java/com/example/demo/product/ProductService.java
@@ -0,0 +1,24 @@
+package com.example.demo.product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class ProductService {
+
+    private ProductRepository productRepository;
+
+    @Autowired
+    ProductService(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+    public Product getProduct(int id) {
+        return productRepository.getProduct(id);
+    }
+
+    public void saveProduct(Product product){
+        productRepository.saveProduct(product);
+    }
+}
